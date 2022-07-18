94459aeae05dafc9208a9bb23ae800b240765985
Adan Martinez <64332675+adanvmartinez@users.noreply.github.com>
2022-07-18 11:42:43 -0500

    Eks

--- main.tf
+++ main.tf
@@ -149,3 +149,27 @@ resource "aws_instance" "terraform_instance" {
     "Name"="terraform-instance"
   }
 }
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "17.24.0"
+  cluster_name    = "Jenkins-cluster"
+  cluster_version = "1.22"
+  subnets         = [aws_subnet.terraform_public_subnet.id]
+
+  vpc_id = module.vpc.vpc_id
+
+  node_groups = {
+      desired_capacity = 3
+      max_capacity     = 3
+      min_capacity     = 3
+  }  
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
