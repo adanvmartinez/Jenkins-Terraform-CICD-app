c671647145c7c9619cabbf3be04fc287c86b5833
Adan Martinez <adanvmartinez@gmail.com>
2022-07-21 23:47:51 -0500

    Fixed typos

--- eks.tf
+++ eks.tf
@@ -26,7 +26,7 @@ resource "aws_eks_cluster" "terraform_cluster" {
 
 # Create AWS EKS Node Group - Public
 resource "aws_eks_node_group" "eks_ng_public" {
-  cluster_name    = aws_eks_cluster.eks_cluster.name
+  cluster_name    = aws_eks_cluster.terraform_cluster.name
 
   node_group_name = "eks-node-group"
   node_role_arn   = aws_iam_role.eks_nodegroup_role.arn
