78fb993f93503ba08f7d734169c6ce470cccd642
Adan Martinez <64332675+adanvmartinez@users.noreply.github.com>
2022-07-25 13:54:17 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>{}, {}!</p>".format(index.get_hello(), index.get_world())
+    return "<p>{}!</p>".format(index.get_hello())
 
 
 @app.route("/adan")
@@ -16,8 +16,8 @@ def adan_route():
     return "<h1>{}<h1>".format(adan.get_message())
 
 def main() -> None:
-    print("{} {}".format(index.get_hello(), index.get_world()))
+    print("{}".format(index.get_hello()))
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 80))
     app.run(debug=True, host='0.0.0.0', port=port)
